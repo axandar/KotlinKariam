ca03517c02c4d93d1a38596d2e2f7b475149c9a2
Axandar <kontakt@michalpiatkowski.com>
2019-02-27 21:37:25 +0100

    Added first model class

--- KotlinKariam-backend/src/com/kotlinkariam/PlayerHandlerRoutes.kt
+++ KotlinKariam-backend/src/com/kotlinkariam/PlayerHandlerRoutes.kt
@@ -1,12 +1,15 @@
+package com.kotlinkariam
+
 import io.ktor.application.call
 import io.ktor.response.respondText
 import io.ktor.routing.Route
+import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.route
 
 fun Route.playerHandler(){
-    route("/playerHandler"){
-        post("/buildOnPlayerInTown/{playerId}/{townId}/{buildableId}"){
+    route("/buildOnPlayerInTown"){
+        get("/{playerId}/{townId}/{buildableId}"){
             val playerId = call.parameters["playerId"]
             val buildableId = call.parameters["buildableId"]
             val townId = call.parameters["townId"]
